85c8f87480c6c2ae3b698c8701887e4947dde9ae
leon-siqueira <leon.siqueir4@gmail.com>
2022-04-19 20:42:40 -0300

    schedulings can be edited and deleted

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :confession_queues do
-    resources :schedulings, only: %i[index new create]
+    resources :schedulings, only: %i[index new create edit update]
   end
 
-  #resources :schedulings, only: %i[new create]
+  resources :schedulings, only: %i[destroy]
 
-  root to: "confession_queues#index"
+  root to: 'confession_queues#index'
 end
